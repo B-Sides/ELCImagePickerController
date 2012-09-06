573f4377b6fd0d981555d67e6d6efffe0a35e2e4
Jack Nutting <jacknutting@mac.com>
2012-09-06 14:38:47 +0200

    Protocol for propagating asset selection messages (instead of typecasting)

---
+++ Classes/ELCAssetSelectionDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ELCAssetSelectionDelegate.h
+//  ELCImagePickerDemo
+//
+//  Created by JN on 9/6/12.
+//  Copyright (c) 2012 ELC Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ELCAssetSelectionDelegate <NSObject>
+
+-(void)selectedAssets:(NSArray*)_assets;
+
+@end
