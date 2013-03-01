a2352843246c6096c301a4039ba2ca9c2a891c61
koogawa <koogawa.app@gmail.com>
2013-03-02 02:14:38 +0900

    'presentModalViewController' is deprecated: 'dismissModalViewControllerAnimated:' is deprecated: first deprecated in iOS 6.0

--- Classes/ELCImagePickerDemoViewController.m
+++ Classes/ELCImagePickerDemoViewController.m
@@ -23,7 +23,7 @@
 	[elcPicker setDelegate:self];
     
     ELCImagePickerDemoAppDelegate *app = (ELCImagePickerDemoAppDelegate *)[[UIApplication sharedApplication] delegate];
-	[app.viewController presentModalViewController:elcPicker animated:YES];
+    [app.viewController presentViewController:elcPicker animated:YES completion:nil];
     [elcPicker release];
     [albumController release];
 }
@@ -32,7 +32,7 @@
 
 - (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info {
 	
-	[self dismissModalViewControllerAnimated:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
 	
     for (UIView *v in [scrollview subviews]) {
         [v removeFromSuperview];
@@ -59,7 +59,7 @@
 
 - (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker {
 
-	[self dismissModalViewControllerAnimated:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
