21782908abdba0f6a37e45396df966219ae48cf0
Arturo <Arturo@MacBook-Pro-de-Arturo.local>
2014-10-21 13:56:13 +0200

    Correction in refreshing the number of photos in album
    Correction in refreshing the number of photos in album
    
    Signed-off-by: Arturo <Arturo@MacBook-Pro-de-Arturo.local>

--- Classes/ELCImagePicker/ELCAlbumPickerController.m
+++ Classes/ELCImagePicker/ELCAlbumPickerController.m
@@ -81,11 +81,18 @@
         
         }
     });
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reloadTableView) name:ALAssetsLibraryChangedNotification object:nil];
     [self.tableView reloadData];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:ALAssetsLibraryChangedNotification object:nil];
 }
 
 - (void)reloadTableView
